567703ef05cc763626a93cd014b4768219f1a59d
Jonathan Arrance <JonathanArrance@users.noreply.github.com>
2020-08-07 20:52:48 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,22 @@ $INTERFACE='wlan0'
 $PRIVATEIP='10.10.10.2'
 
 #pre-reqs
-apt update
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get autoremove -y
 
 #set up the private ip addr
-ip addr add $PRIVATEIP/24 dev $INTERFACE
-echo 'ip addr add $PRIVATEIP/24 dev $INTERFACE' 
+sudo ip addr add $PRIVATEIP/24 dev $INTERFACE
+sudo echo 'ip addr add $PRIVATEIP/24 dev $INTERFACE' 
 
+#install the packages
+#Install hostapd and enable at startup
+sudo apt install -y hostapd
+sudo systemctl unmask hostapd
+sudo systemctl enable hostapd
+
+#Install dnsmasq
+sudo apt install -y dnsmasq
+
+#Install netfilter and iptables
+sudo DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent
