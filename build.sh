fde230b93f370c563ffdcf909062636a84cbd664
Jonathan Arrance <JonathanArrance@users.noreply.github.com>
2020-08-07 21:17:25 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,13 @@ $PRIVATEIP='10.10.10.2'
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get autoremove -y
+sudo adpt install -y git
+sudo apt install -y curl
+sudo apt install -y wget
+
+#set up python3
+sudo rm /usr/bin/python
+sudo ln -s /usr/bin/python3.7 /usr/bin/python
 
 #set up the private ip addr
 sudo ip addr add $PRIVATEIP/24 dev $INTERFACE
@@ -23,3 +30,5 @@ sudo apt install -y dnsmasq
 
 #Install netfilter and iptables
 sudo DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent
+
+#set up dhcpd.conf - set up perm IP for pi-point
